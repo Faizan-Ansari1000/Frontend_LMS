88ee146fba8fcd2ecd8914f2f6edee62828af77c
Faizan-Ansari <ansarizada70@gmail.com>
2025-01-19 09:03:24 -0800

    Update SignUp.js
    change the UI

--- src/Auth/SignUp.js
+++ src/Auth/SignUp.js
@@ -42,7 +42,7 @@ export default function SignUp() {
             {/* Sir ke liye special message */}
             <div className="absolute top-0 left-0 right-0 bg-cyan-400 text-black text-center py-4">
                 <Text className="text-lg font-semibol">
-                    This project wouldn't have been possible without the guidance of my mentor, Sir Abdul Basit Ahmed (Sr. MERN Stack & React Native Developer).<Link to={"/Admin"}>Admin</Link><Link to={"/StudentPortal"}>Student</Link>
+                    This project wouldn't have been possible without the guidance of my mentor, Sir Abdul Basit Ahmed (Sr. MERN Stack & React Native Developer).
                 </Text>
             </div>
             
@@ -54,6 +54,8 @@ export default function SignUp() {
                 )}
                 <div className="w-full sm:w-96 bg-white p-8 rounded-lg shadow-2xl m-2">
                     <h2 className="text-2xl font-semibold text-center text-cyan-700 mb-6">Sign Up</h2>
+                    <Link to={"/Admin"} className={"text-start me-5"}>Admin</Link>
+                    <Link to={"/StudentPortal"} className={"text-end ms-5"}>Student</Link>
                     <div className="mb-4">
                         <Input
                             onChange={(e) => setModel({ ...model, name: e.target.value })}
